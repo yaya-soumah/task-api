982ede50f7d48276aca8a34d85ea0bfb1d1928e9
yaya-soumah <yaya-soumah@outlook.com>
2025-03-19 10:42:53 +0800

    Switch to postgres for Render

--- task_project/task_project/settings.py
+++ task_project/task_project/settings.py
@@ -80,9 +80,7 @@ WSGI_APPLICATION = 'task_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///'+os.path.join(BASE_DIR,'db.sqlite3')
-    )
+    'default': dj_database_url.config(conn_max_age=600, default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
 }
 
 
